b6517b11d0da12972ef3e5ec3cf25e1b5306b60f
CodeBear <249900679@qq.com>
2026-01-09 00:28:27 +0800

    chore: 清理dist目录并更新.gitignore文件
    移除dist目录下的所有生成文件
    将dist目录添加到.gitignore中

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './core/types';
-export * from './providers/lt';
-import { LTProvider } from './providers/lt';
-export { LTProvider as LTSdk };
--- dist/providers/lt/types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// ================= AIGC 接口参数 =================
-Object.defineProperty(exports, "__esModule", { value: true });
